880b6d905e62edfc8906417e744dab9ad27979e4
713068_Ashim <ashimaakash1997@gmail.com>
2025-02-02 13:09:44 +0530

    created Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@
 
 const App = () => {
     return (
-        <div><h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1></div>
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="../src/assets/hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You&#39;ll Enjoy Without the Hassle</h1>
+                </header>
+                {/*<p>Search</p>*/}
+            </div>
+        </main>
     )
 }
 export default App
