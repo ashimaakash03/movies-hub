d9a777e72ffebbd58b4089f48e30d2fcea9dd0e2
713068_Ashim <ashimaakash1997@gmail.com>
2025-02-08 21:34:22 +0530

    fetched movies data into console using async function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 //import React from 'react'
 
 import Search from "./components/Search.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+    }
+}
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [errorMessage, setErrorMessage] = useState(null);
 
+    const fetchMovies = async () => {
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const response = await fetch(endpoint, API_OPTIONS);
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log(`Error fetching movies ${error}`);
+            setErrorMessage('Error fetching movies. Please try again later.');
+        }
+    }
+    useEffect(() => {
+        fetchMovies();
+    }, [])
     return (
         <main>
             <div className="pattern"/>
@@ -13,8 +39,11 @@ const App = () => {
                 <header>
                     <img src="../src/assets/hero.png" alt="Hero Banner"/>
                     <h1>Find <span className="text-gradient">Movies</span> You&#39;ll Enjoy Without the Hassle</h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                 </header>
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <section className='all-movies'>
+                    <h2>All Movies...</h2>{errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+                </section>
             </div>
         </main>
     )
